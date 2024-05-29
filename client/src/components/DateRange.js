fa4cc9649cf3f3949ded4cdae13e4f9fb17cc173
sandking-number1 <Alfred.J9412@gmail.com>
2024-05-29 14:22:50 -0500

    Creating custom date range

---
+++ client/src/components/DateRange.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import moment from "moment";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Grid from "@mui/material/Grid";
+
+const _ = require("lodash");
+const boy = moment("01/01/2024");
+
+export default function DateRange(props) {
+    const [clearedMin, setClearedMin] = useState(false);
+    const [clearedMax, setClearedMax] = useState(false);
+
+    useEffect(() => {
+        if (clearedMin) {
+            setClearedMin(false);
+            updateDates("min", null);
+        }
+    }, [setClearedMin]);
+
+    useEffect(() => {
+        if (clearedMax) {
+            setClearedMax(false);
+            updateDates("max", null);
+        }
+    }, [clearedMax]);
+
+    const updateDates = (date, value) => {
+        const tempSelectedDates = _.cloneDeep(props.selectedDates);
+        tempSelectedDates[date] = value ? moment(value) : null;
+        props.setSelectedDates(tempSelectedDates);
+    };
+
+    return (
+        <>
+            <Grid item xs={6}>
+                <DatePicker
+                    id="min-date"
+                    label="Min Date"
+                    disableFuture
+                    minDate={boy}
+                    value={props.selectedDates.min}
+                    onChange={(value) => {
+                        updateDates("min", value);
+                    }}
+                    views={["month", "year"]}
+                    slotProps={{
+                        field: {
+                            clearable: true,
+                            onClear: () => setClearedMin(true),
+                        },
+                    }}
+                />
+            </Grid>
+            <Grid item xs={6}>
+                <DatePicker
+                    id="max-date"
+                    label="Max Date"
+                    disableFuture
+                    minDate={props.selectedDates.min || boy}
+                    value={props.selectedDates.max}
+                    onChange={(value) => {
+                        updateDates("max", value);
+                    }}
+                    views={["month", "year"]}
+                    slotProps={{
+                        field: {
+                            clearable: true,
+                            onClear: () => setClearedMin(true),
+                        },
+                    }}
+                />
+            </Grid>
+        </>
+    );
+}
--- client/src/components/SideFilter.js
+++ client/src/components/SideFilter.js
@@ -6,6 +6,7 @@ import Grid from "@mui/material/Grid";
 import axios from "axios";
 
 import PeriodSelector from "./PeriodSelector";
+import DateRange from "./DateRange";
 
 const _ = require("lodash");
 const boy = moment("01/01/2024");
@@ -15,26 +16,12 @@ export default function SideFilter(props) {
         min: null,
         max: null,
     });
-    const [clearedMin, setClearedMin] = useState(false);
+
     const [clearedMax, setClearedMax] = useState(false);
 
     const addTabToURL = (tab) => {
         return `/${tab}/${props.period}`;
     };
-
-    useEffect(() => {
-        if (clearedMin) {
-            setClearedMin(false);
-            updateDates("min", null);
-        }
-    }, [setClearedMin]);
-
-    useEffect(() => {
-        if (clearedMax) {
-            setClearedMax(false);
-            updateDates("max", null);
-        }
-    }, [clearedMax]);
 
     useEffect(() => {
         props.setData(null);
@@ -59,80 +46,32 @@ export default function SideFilter(props) {
         axios.get(url).then((response) => {
             props.setData(response.data);
         });
-    }, [selectedDates, props.period, props.selectedTab]);
-
-    const updateDates = (date, value) => {
-        const tempSelectedDates = _.cloneDeep(selectedDates);
-        tempSelectedDates[date] = value ? moment(value) : null;
-        setSelectedDates(tempSelectedDates);
-    };
+    }, [selectedDates, props.period, props.selectedTab, props.selectedInsight]);
 
     return (
-        <>
-            <Box
-                sx={{
-                    border: "3px solid #2380BF",
-                    borderRadius: 15,
-                    marginTop: 5,
-                }}
-            >
-                <Box sx={{ flexGrow: 1, margin: "8%" }}>
-                    <Grid container spacing={1}>
-                        <Grid item xs={6}>
-                            <DatePicker
-                                id="min-date"
-                                label="Min Date"
-                                disableFuture
-                                minDate={boy}
-                                value={selectedDates.min}
-                                onChange={(value) => {
-                                    updateDates("min", value);
-                                }}
-                                views={["month", "year"]}
-                                slotProps={{
-                                    field: {
-                                        clearable: true,
-                                        onClear: () => setClearedMin(true),
-                                    },
-                                }}
-                                // slotProps={
-                                //     {
-                                //         field: { clearable: true, onClear: () => setCleared(true) },
-                                //     }
-                                // }
-                            />
-                        </Grid>
-                        <Grid item xs={6}>
-                            <DatePicker
-                                id="max-date"
-                                label="Max Date"
-                                disableFuture
-                                minDate={selectedDates.min || boy}
-                                value={selectedDates.max}
-                                onChange={(value) => {
-                                    updateDates("max", value);
-                                }}
-                                views={["month", "year"]}
-                                slotProps={{
-                                    field: {
-                                        clearable: true,
-                                        onClear: () => setClearedMin(true),
-                                    },
-                                }}
-                                // slotProps={
-                                //     {
-                                //         field: { clearable: true, onClear: () => setCleared(true) },
-                                //     }
-                                // }
-                            />
-                        </Grid>
-                    </Grid>
-                    <PeriodSelector
-                        period={props.period}
-                        setPeriod={props.setPeriod}
-                    />
-                </Box>
+        <Box
+            sx={{
+                border: "3px solid #2380BF",
+                borderRadius: 15,
+                marginTop: 5,
+            }}
+        >
+            <Box sx={{ flexGrow: 1, margin: "8%" }}>
+                <Grid container spacing={1}>
+                    {(props.selectedTab === 0 ||
+                        (props.selectedTab === 1 &&
+                            props.selectedInsight === 0)) && (
+                        <DateRange
+                            selectedDates={selectedDates}
+                            setSelectedDates={setSelectedDates}
+                        />
+                    )}
+                </Grid>
+                <PeriodSelector
+                    period={props.period}
+                    setPeriod={props.setPeriod}
+                />
             </Box>
-        </>
+        </Box>
     );
 }
